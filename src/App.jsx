6bd9ada95c9c4ff37a1460b92ac18c1548ac9f1d
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-12-14 15:51:03 +0100

    capture the last keypressed and render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
+import {useEffect, useState} from "react"
 
+
+function captureKeystroke(e){
+const input = e.target.value;
+return input.slice(-1)
+}
 function App() {
+  const [input, setInput] = useState(()=>[])
+  useEffect(() => {
+    console.log(input)
+    
+  }, [input])
   return (
     <div>
-      MY AWSOME TERMINAL WEBSITE!
+      <textarea onChange={(e)=> {
+      setInput((prev) => [...prev,captureKeystroke(e)])}} />
+      <p>{`guest@terminal:$ ${input.join("")}`}</p>
    </div>
   )
 }
