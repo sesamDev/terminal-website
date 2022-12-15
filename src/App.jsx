2e696afe23d4450eaf0c6b6854f32b600245b280
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-12-15 10:43:47 +0100

    new lines added when enter is pressed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,74 @@
-import './App.css'
-import {useEffect, useState} from "react"
+import "./App.css";
 
+import { useEffect, useState } from "react";
 
-function captureKeystroke(e){
-const input = e.target.value;
-return input.slice(-1)
+function captureKeystroke(e) {
+  const input = e.target.value;
+  return input;
 }
-function App() {
-  const [input, setInput] = useState(()=>[])
+
+function Prompt(props) {
+  const [input, setInput] = useState(() => "");
+  let active = props.active;
+  const user = "guest@terminal:~$";
+
   useEffect(() => {
-    console.log(input)
-    
-  }, [input])
+    if (active) {
+      setInput(props.input);
+    }
+  }, [props.input]);
+
+  if (props.depth !== 0)
+    return (
+      <>
+        {(active = false)}
+        <p>{`${user} ${input}`}</p>
+        <Prompt active={true} input={props.input} depth={props.depth - 1} />
+      </>
+    );
+
   return (
-    <div>
-      <textarea onChange={(e)=> {
-      setInput((prev) => [...prev,captureKeystroke(e)])}} />
-      <p>{`guest@terminal:$ ${input.join("")}`}</p>
-   </div>
-  )
+    <>
+      <p>{`${user} ${input}`}</p>
+    </>
+  );
 }
 
-export default App
+function App() {
+  const [input, setInput] = useState(() => "");
+  const [numberOfLines, setNumberOfLines] = useState(() => 0);
+  console.log(numberOfLines);
+
+  function handleEnterKey(e) {
+    if (e.key !== "Enter") return;
+    const textArea = document.querySelector("textarea");
+    textArea.value = "";
+
+    setNumberOfLines((prev) => ++prev);
+
+    return console.log(e.key);
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      handleEnterKey(e);
+    });
+    return () => {
+      // document.removeEventListener("keydown", (e) => {
+      //   handleEnterKey(e);
+      // });
+    };
+  }, []);
+  return (
+    <div className="mx-4">
+      <textarea
+        onChange={(e) => {
+          setInput(captureKeystroke(e));
+        }}
+      />
+      <Prompt active={true} input={input} depth={numberOfLines} />
+    </div>
+  );
+}
+
+export default App;
