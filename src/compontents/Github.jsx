a8da8eb218b77a6f0e63be28d5eca2fc867855e4
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-12-17 15:46:27 +0100

    changed link to the repo of the site

--- src/compontents/Github.jsx
+++ src/compontents/Github.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'React'
+import React, { useState, useEffect } from 'react'
 
 const Github = () => {
   const [loading, setLoading] = useState(true)
   useEffect(() => {
     setTimeout(() => {
-      window.open('http://github.com/sesamDev', '_blank')
+      window.open('http://github.com/sesamDev/terminal-website', '_blank')
       setLoading(false)
     }, 2000)
   }, [])
